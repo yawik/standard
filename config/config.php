ed92fac4661ddb16f2bfac5fc8371dd665a182d1
Anthonius Munthi <me@itstoni.com>
2018-10-25 13:33:19 +0800

    fixed configurations

--- config/config.php
+++ config/config.php
@@ -1,13 +1,46 @@
 <?php
+
 /**
  * If you need an environment-specific system or application configuration,
  * there is an example in the documentation
  * @see https://docs.zendframework.com/tutorials/advanced-config/#environment-specific-system-configuration
  * @see https://docs.zendframework.com/tutorials/advanced-config/#environment-specific-application-configuration
  */
+
+$modules = require __DIR__.'/modules.config.php';
+
+if (!file_exists(__DIR__ . '/autoload/yawik.config.global.php')) {
+    $modules = [
+        'Install',
+        'Core',
+        'Auth',
+        'Jobs',
+    ];
+}else{
+    foreach (glob(__DIR__ . '/autoload/*.module.php') as $moduleFile) {
+        $addModules = require $moduleFile;
+        foreach ($addModules as $addModule) {
+            if (strpos($addModule, '-') === 0) {
+                $remove = substr($addModule, 1);
+                $modules = array_filter($modules, function ($elem) use ($remove) {
+                    return strcasecmp($elem, $remove);
+                });
+            } else {
+                if (!in_array($addModule, $modules)) {
+                    $modules[] = $addModule;
+                }
+            }
+        }
+    }
+}
+
+$env = getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV'):'development';
+
+$modules = \Core\Yawik::generateModuleConfiguration($modules);
+
 return [
     // Retrieve list of modules used in this application.
-    'modules' => require __DIR__ . '/modules.config.php',
+    'modules' => $modules,
 
     // These are various options for the listeners attached to the ModuleManager
     'module_listener_options' => [
@@ -30,21 +63,21 @@ return [
         // Whether or not to enable a configuration cache.
         // If enabled, the merged configuration will be cached and used in
         // subsequent requests.
-        'config_cache_enabled' => true,
+        'config_cache_enabled' => ($env === 'production'),
 
         // The key used to create the configuration cache file name.
-        'config_cache_key' => 'application.config.cache',
+        'config_cache_key' => $env,
 
         // Whether or not to enable a module class map cache.
         // If enabled, creates a module class map cache which will be used
         // by in future requests, to reduce the autoloading process.
-        'module_map_cache_enabled' => true,
+        'module_map_cache_enabled' => ($env === 'production'),
 
         // The key used to create the class map cache file name.
         'module_map_cache_key' => 'application.module.cache',
 
         // The path in which to cache merged configuration.
-        'cache_dir' => __DIR__.'/../var/cache',
+        'cache_dir' => realpath(dirname(__DIR__)).'/var/cache',
 
         // Whether or not to enable modules dependency checking.
         // Enabled by default, prevents usage of modules that depend on other modules
--- config/modules.config.php
+++ config/modules.config.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
  * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
@@ -10,9 +11,8 @@
  *
  * This should be an array of module namespaces used in the application.
  */
-use Core\Bootstrap;
 
-return Bootstrap::generateModuleConfiguration([
+return [
     'Core',
     'Settings',
     'Pdf',
@@ -22,6 +22,5 @@ return Bootstrap::generateModuleConfiguration([
     'Cv',
     'Auth',
     'Applications',
-    'YawikDemoSkin',
     'Demo'
-]);
+];
